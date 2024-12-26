fb3944837256b80dc43103b5e31f05548929955e
Matej Topolovac <>
2024-12-26 12:47:21 +0100

    rate_my_lick: added prometheus

--- rate_my_lick/main.go
+++ rate_my_lick/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"log"
+
 	"github.com/google/uuid"
 	"github.com/gorilla/sessions"
+	"github.com/labstack/echo-contrib/echoprometheus"
 	"github.com/labstack/echo-contrib/session"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/topolovac/learning_projects/rate_my_lick/services"
 )
@@ -31,6 +35,22 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(session.Middleware(sessions.NewCookieStore([]byte("secret"))))
 
+	customCounter := prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "custom_requests_total",
+			Help: "How many HTTP requests processed, partitioned by status code and HTTP method.",
+		},
+	)
+	if err := prometheus.Register(customCounter); err != nil {
+		log.Fatal(err)
+	}
+
+	e.Use(echoprometheus.NewMiddlewareWithConfig(echoprometheus.MiddlewareConfig{
+		AfterNext: func(c echo.Context, err error) {
+			customCounter.Inc()
+		},
+	}))
+
 	e.Static("/static", "static")
 
 	e.Use(CreateSession)
@@ -43,6 +63,8 @@ func main() {
 	e.GET("/my-licks", app.MyLicksPageHandler)
 
 	e.POST("/lick/:id/rate/:rate", app.RateLickHandler)
+
+	e.GET("/metrics", echoprometheus.NewHandler())
 
 	err := e.Start(":3000")
 	e.Logger.Fatal(err)
